94e481aaeb0b90ef7497b7d14d3bf8ac90482450
David Miranda <david@switchpayments.com>
2020-01-13 19:31:45 +0000

    Add database model

--- places/models.py
+++ places/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    place_id = models.CharField(primary_key=True, max_length=200, unique=True, db_index=True)
+    name = models.CharField(max_length=200, db_index=True)
